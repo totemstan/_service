9601007b09c9373baebaf021d366511e43b2117c
totemstan <brian.d.james@comcast.net>
2022-02-07 12:26:16 -0500

    redoc

--- testagent.js
+++ testagent.js
@@ -1,3 +1,9 @@
+/**
+To add your compute agents to TOTEM's compute cloud, simply register your agent 
+using the following code pattern.  In the example below, `add` and `cat` 
+agents are listening on port 3333.
+*/
+
 require("http").get("http://localhost:8080/agent?port=3333", res => {
 	var msg = "";
 	res.on("data", data => msg += data.toString());
