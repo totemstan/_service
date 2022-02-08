e697d4dd96a4cc6a85402c1a362073174b6ec72d
totemstan <brian.d.james@comcast.net>
2022-02-08 14:37:31 -0500

    redoc

--- testagent.js
+++ testagent.js
@@ -1,29 +1,66 @@
+// UNCLASSIFIED
 /**
 To add your compute agents to TOTEM's compute cloud, simply register your agent 
-using the following code pattern.  In the example below, `add` and `cat` 
-agents are listening on port 3333.
+using the code pattern below which, here, listens for `add, `cat`, `/me.js` and
+`dft` requests on port 3333.
 */
 
-require("http").get("http://localhost:8080/agent?port=3333", res => {
-	var msg = "";
-	res.on("data", data => msg += data.toString());
+const 
+	need$ = process.argv[2] ? true : false,
+	$ = need$ ? require("./man") : null, // "/mnt/public/totem/repo/man",
+	$fs = need$ ? require("fs") : null,
+	totems = {
+		RLE: "https://RLENET.126:8443",
+		WWW: "http://totem.hopto.org",
+		SBU: "https://totem.nga.mil:8443",
+		COE: "https://totem.coe.nga.mil",
+		LOCAL: "http://localhost:8080"
+	},
+	totem = totems.LOCAL,
+	port = 3333,
+	agents = {	// define your agents here
+		add: (req,res) => {	// an agent to add x and y
+			const 
+				{x,y} = req.query,
+				calc = (x,y) => x+y;
+
+			res( calc(x,y) );
+		},
+		cat: (req,res) => {	// an agent to concatenate x with y
+			const 
+				{x,y} = req.query,
+				calc = (x,y) => [x,y];
+
+			res( calc(x,y) );
+		},
+		"/me.js": (req,res) => {	// send a file
+			res( path => {
+				try {
+					$fs.readFile( "./testagent.js", "utf-8", (err,txt) => res(txt) );
+				}
+				catch (err) {
+					res("Error: file not found");
+				}
+			});
+	},
+		"dft": (req,res) => {	// fft of post `x` array + query `a` constant 
+			const
+				{ y } = ctx = $("y=dft(x+a)", {		// context for $
+					x: req.body.x || [0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0],
+					a: req.query.a
+				});
+
+			res( y.get("re&im") );
+		}
+	};
+
+// do not alter
+require("http").get(`${totem}/agent?port=${port}&keys=${Object.keys(agents)}`, res => {
+	var agent = "";
+	res.on("data", data => agent += data.toString());
 	res.on("end", () => {
-		eval(msg);
-		myAgent({	// define your agents here
-			add: (req,res) => {	// an agent to add x and y
-				const 
-					{x,y} = req.query,
-					calc = (x,y) => x+y;
-
-				res( calc(x,y) );
-			},
-			cat: (req,res) => {	// an agent to concatenate x with y
-				const 
-					{x,y} = req.query,
-					calc = (x,y) => [x,y];
-
-				res( calc(x,y) );
-			}			
-		});
+		eval(agent);
 	});
 }).end();
+
+// UNCLASSIFIED
